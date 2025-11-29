0c58c59a5eb8a5285e8a8cc64d5a93ff42c03be1
nciganovic <nciganovic99@gmail.com>
2025-11-29 19:39:45 +0100

    add type command

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import sys
 
+built_in_commands = ['echo', 'exit', 'type']
 
 def main():
     # TODO: Uncomment the code below to pass the first stage
@@ -8,9 +9,16 @@ def main():
         user_input = input()
         elements: List[str] = user_input.split(' ')
         command = elements[0]
+        args:List[str] = elements[1:]
 
         if(command == 'echo'):
             print(user_input[5:])
+        elif(command == 'type'):
+            for a in args:
+                if a in built_in_commands:
+                    print(f'{a} is a shell builtin')
+                else:
+                    print(f'{a} not found')
         elif(command == 'exit'):
             sys.exit()
         else:
