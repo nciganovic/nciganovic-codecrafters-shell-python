039c392bc3dc4ae4ee3ee7671d865f52a39443f0
nciganovic <nciganovic99@gmail.com>
2025-11-29 19:24:21 +0100

    add echo command

--- app/main.py
+++ app/main.py
@@ -5,12 +5,16 @@ def main():
     # TODO: Uncomment the code below to pass the first stage
     while True:
         sys.stdout.write("$ ")
-        command = input()
-        
-        if(command == 'exit'):
+        user_input = input()
+        elements: List[str] = user_input.split(' ')
+        command = elements[0]
+
+        if(command == 'echo'):
+            print(user_input[5:])
+        elif(command == 'exit'):
             sys.exit()
-        
-        print(f'{command}: command not found')
+        else:
+            print(f'{command}: command not found')
 
 
 if __name__ == "__main__":
