ef2229fce5238a0a8db1c0b9183287e5aec335cc
nciganovic <nciganovic99@gmail.com>
2026-01-19 21:48:44 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,16 +1,31 @@
 import sys
 import os 
+import subprocess
 
 built_in_commands = ['echo', 'exit', 'type']
 
 def main():
-    # TODO: Uncomment the code below to pass the first stage
     while True:
         sys.stdout.write("$ ")
         user_input = input()
         elements: List[str] = user_input.split(' ')
         command = elements[0]
         args:List[str] = elements[1:]
+
+        #Check in PATH
+        PATH = os.environ.get("PATH")
+        all_paths = PATH.split(os.pathsep)
+        execute_allowed = False
+        for path in all_paths:
+            full_path = path + "/" + command
+            if os.path.exists(full_path) and os.access(full_path, os.X_OK):
+                execute_allowed = True
+                break
+        if execute_allowed:
+            commands = [command]
+            commands.extend(args)
+            subprocess.run(commands)
+            continue
 
         if(command == 'echo'):
             print(user_input[5:])
@@ -35,6 +50,8 @@ def main():
             sys.exit()
         else:
             print(f'{command}: command not found')
+
+            
 
 
 if __name__ == "__main__":
